ccd63d44163437a6e9cf415a78012806ce85821a
GraySpider <76154662@qq.com>
2016-10-05 16:59:31 +0800

    修复了重复运行创建数据库表时候重复创建带来的报错

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('chat.db');
 
 function createChatDatabase() {
-  db.run("create table users(" +
+  db.run("CREATE TABLE IF NOT EXISTS users(" +
     "id INTEGER PRIMARY KEY," +
     "username VARCHAR(255) NOT NULL," +
     "password VARCHAR(255) NOT NULL," +
@@ -10,13 +10,13 @@ function createChatDatabase() {
     "portrait VARCHAR(255) NOT NULL" +
     ")"
   );
-  db.run("CREATE TABLE user_friends(" +
+  db.run("CREATE TABLE IF NOT EXISTS user_friends(" +
     "id INTEGER PRIMARY KEY," +
     "from_userid INTEGER," +
     "to_userid INTEGER" +
     ")"
   );
-  db.run("CREATE TABLE user_message(" +
+  db.run("CREATE TABLE IF NOT EXISTS user_message(" +
     "id INTEGER PRIMARY KEY," +
     "from_userid INTEGER," +
     "to_userid INTEGER," +
@@ -26,4 +26,10 @@ function createChatDatabase() {
   )
 }
 
+function insertUser(username, password, name, portrait) {
+  db.run("INSERT INTO users VALUES (?,?,?,?,?)", [null, username, password, name, portrait]);
+}
+
+
+exports.insertUser = insertUser;
 exports.createChatDatabase = createChatDatabase;
