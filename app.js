82b0e4350c134a7a88e9afd7c86bc71844d29f52
GraySpider <76154662@qq.com>
2016-10-06 20:29:56 +0800

    增加了chat和login的路由

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
 var app = express();
+var login = require('./router/login');
+var chat = require('./router/chat');
 
 app.engine('html', ejs.__express);
 
@@ -9,6 +11,9 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(login);
+app.use(chat);
 
 app.listen(8080, function () {
   console.log("http://localhost:8080");
